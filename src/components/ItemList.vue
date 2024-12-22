3455048ffda870b7c2398a12b57f66a194720ad5
narathip1598 <narathip@blendata.co>
2024-12-22 09:38:23 +0700

    Test

---
+++ src/components/ItemList.vue
@@ -0,0 +1,34 @@
+<!-- src/components/ItemList.vue -->
+<template>
+  <div>
+    <h1>Items List</h1>
+    <ul>
+      <li v-for="item in items" :key="item.id">{{ item.name }} - {{ item.description }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  created() {
+    this.fetchItems();
+  },
+  methods: {
+    async fetchItems() {
+      try {
+        const response = await axios.get('http://localhost:8000/api/items/');
+        this.items = response.data;
+      } catch (error) {
+        console.error('Error fetching items:', error);
+      }
+    },
+  },
+};
+</script>
